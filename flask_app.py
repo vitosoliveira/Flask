b55e1c94f6c1929db36e4a94c2ee87ce769950ca
Vitor Soares <vso1997@gmail.com>
2020-03-04 23:27:41 +0000

    Commit

---
+++ flask_app.py
@@ -0,0 +1,12 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1> Primeiro Flask de idiota </h1>'
+def bom_dia():
+    return "<title> Vem cambada </title>"
